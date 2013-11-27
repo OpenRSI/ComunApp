1a742c3e080f41a55b4eb27db4691f4f587c5abd
Boris STOCKER <boris.stocker@openrsi.fr>
2013-11-27 03:16:53 -0600

    Changes made by Boris STOCKER (boris.stocker@openrsi.fr).

---
+++ ComunApp/js/twitter.js
@@ -0,0 +1,20 @@
+//chargement complet de la page
+$(document).bind('pageinit', function() {
+    
+        
+    //----Gestion des boutons Menu--------------------------->
+    
+    //----Menu Header ------------->
+    
+    $("#MENUHT").bind("click",function(){
+        window.location = "index.html";
+    });
+    
+    
+    //------------------------------------------------------>
+    
+    
+    
+    
+    
+});

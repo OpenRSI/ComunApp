ae5540f4351e9c0e29b692a6bd500442d6364fc5
Philippe TARRALLE <philippe.tarralle@openrsi.fr>
2013-12-02 14:59:52 -0600

    Changes made by Philippe TARRALLE (philippe.tarralle@openrsi.fr).

--- ComunApp/ecrit_cache.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-    require_once("feedparser.php"); //echo FeedParser("http://www.vivre-a-niort.com/fr/news.xml",10);
-	require_once("flux.php");
-	$essai=FeedParser($flux1,10);
-
-	$fic=fopen("./cache_local/cache.txt", "w");
-	fwrite($fic,$essai);
-	fclose($fic);
-?>	
--- ComunApp/flux.php
+++
@@ -1,3 +0,0 @@
-﻿<?php
-$flux1 = ("http://www.vivre-a-niort.com/fr/news.xml");
-?>
